e5f48e704661cb9a80e67f862a7b1d3bc2a4d639
Miemie17 <jamielazaro17@gmail.com>
2024-10-14 16:22:20 +0800

    feat: Lesson_2

---
+++ src/Lecture_2/Hello.java
@@ -0,0 +1,11 @@
+package Lecture_2;
+
+public class Hello {
+    public static void main (String[] args){
+        System.out.println("Hello");
+        doSomething();
+    }
+    public static void doSomething(){
+        System.out.println("Do");
+    }
+}
---
+++ src/Lecture_2/Test.java
@@ -0,0 +1,8 @@
+package Lecture_2;
+
+public class Test {
+    public static void main(String[] args) {
+        //comment
+
+    }
+}

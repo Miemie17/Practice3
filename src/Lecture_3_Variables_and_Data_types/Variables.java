69aad2163d28168ae9be1a064824768379a4f8e4
Miemie17 <jamielazaro17@gmail.com>
2024-10-14 17:21:05 +0800

    feat: Lecture 3 Variables and Data Types

---
+++ src/Lecture_3_Variables_and_Data_types/Variables.java
@@ -0,0 +1,18 @@
+package Lecture_3_Variables_and_Data_types;
+
+public class Variables {
+    public static void main(String[] args) {
+        int x = 34;
+        System.out.println(x);
+
+        x=55;
+        System.out.println(x);
+        System.out.println(x+45);
+
+        String sentence= "This is a sample sentence.";
+        System.out.println(sentence);
+        System.out.println(sentence+" I added soem characters to the existing string");
+
+
+    }
+}

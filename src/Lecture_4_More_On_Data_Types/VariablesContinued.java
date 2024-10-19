28b2933135ed5f5ab7237f430f70ba9773b42a95
Miemie17 <jamielazaro17@gmail.com>
2024-10-19 10:49:34 +0800

    feat: Lecture_4_More_On_Data_Types

---
+++ src/Lecture_4_More_On_Data_Types/VariablesContinued.java
@@ -0,0 +1,18 @@
+package Lecture_4_More_On_Data_Types;
+
+public class VariablesContinued {
+    public static void main(String[] args) {
+        //int can take values up to 30 bits
+        int var = 1000000000;
+        int negVar = -1000000000;
+        long bigNumber = 1000000000000L;
+        byte reallySmallNumber = 127;
+        byte reallySmallNegativeNumber = -128;
+        double decimalVariable = 394.003;
+        boolean decisionTrue = true;
+        boolean decisionFalse = false;
+        char charLetter = 'L';
+        char charSpecial = '%';
+        char charNumber = '1';
+    }
+}
